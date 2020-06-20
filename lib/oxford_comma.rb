f06a3f4661fc8b7cabd8dca276f2ae9fe7155920
zachary mines <zmines@wesleyan.edu>
2020-06-20 20:29:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2 
+    return "#{array[0]} and #{array[1]}"
+  else 
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
+  return array
 end
